83159ae40ca13b1826cb04e74663436708ad41c0
Karina Kiselyova <kimitoshiko@gmail.com>
2024-04-27 22:37:22 +0500

    fix: garage block bottom padding

--- async-race-react-app/src/App.js
+++ async-race-react-app/src/App.js
@@ -6,14 +6,14 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
 require("./App.css");
-var _Winners = _interopRequireDefault(require("./TSComponents/Winners"));
+var _Garage = _interopRequireDefault(require("./TSComponents/Garage"));
 var _Navbar = _interopRequireDefault(require("./TSComponents/Navbar"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-// import Garage from './TSComponents/Garage';
+// import Winners from './TSComponents/Winners';
 
 function App() {
   return /*#__PURE__*/_react.default.createElement("div", {
     className: "App"
-  }, /*#__PURE__*/_react.default.createElement(_Navbar.default, null), /*#__PURE__*/_react.default.createElement(_Winners.default, null));
+  }, /*#__PURE__*/_react.default.createElement(_Navbar.default, null), /*#__PURE__*/_react.default.createElement(_Garage.default, null));
 }
 var _default = exports.default = App;
--- async-race-react-app/src/App.tsx
+++ async-race-react-app/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-// import Garage from './TSComponents/Garage';
-import Winners from './TSComponents/Winners';
+import Garage from './TSComponents/Garage';
+// import Winners from './TSComponents/Winners';
 import Navbar from './TSComponents/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Garage /> */}
-      <Winners />
+      <Garage />
+      {/* <Winners /> */}
     </div>
   );
 }

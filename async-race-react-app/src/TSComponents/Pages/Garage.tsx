68008a5b343d26e7cc8b78f372891db57f84eb8a
Karina Kiselyova <kimitoshiko@gmail.com>
2024-05-01 01:47:53 +0500

    feat: add generation of random 100 cars on btn click

--- async-race-react-app/src/TSComponents/Pages/Garage.tsx
+++ async-race-react-app/src/TSComponents/Pages/Garage.tsx
@@ -1,12 +1,29 @@
 import '../../garage.css';
 import CarList from '../CarList';
 import useFetch from '../useFetch';
+import axios from 'axios';
+import ICar from '../Interfaces/ICar';
+import CarBrandList from '../Data/CarBrandList';
+import CarModelList from '../Data/CarModelList';
+import HexAlphabetList from '../Data/HexAlphabetList';
 
 type Props = {};
 
 const Garage = (props: Props) => {
     const carsUrl: string = 'http://localhost:3000/garage'; 
     const { data: cars, isPending, error } = useFetch(carsUrl);
+
+    const handleGenerateRandomCars: VoidFunction = () => {
+        for (let i: number = cars.length; i < cars.length + 100; i++) {
+            let newCar: ICar = {
+                "name": `${CarBrandList[Math.round(Math.random()*(CarBrandList.length -1))]} ${CarModelList[Math.round(Math.random()*(CarModelList.length -1))]}`,
+                "color": `#${HexAlphabetList[Math.round(Math.random()*(HexAlphabetList.length-1))]}${HexAlphabetList[Math.round(Math.random()*(HexAlphabetList.length-1))]}${HexAlphabetList[Math.round(Math.random()*(HexAlphabetList.length-1))]}${HexAlphabetList[Math.round(Math.random()*(HexAlphabetList.length-1))]}${HexAlphabetList[Math.round(Math.random()*(HexAlphabetList.length-1))]}${HexAlphabetList[Math.round(Math.random()*(HexAlphabetList.length-1))]}`,
+                "id": i+1
+            };
+
+            axios.post(carsUrl, newCar);
+        }
+    }
 
     return (
         <div className="garage">
@@ -29,7 +46,7 @@ const Garage = (props: Props) => {
                         </div>
                     </div>
                     <div className="generate-cars-btn">
-                        <button className='light-blue-btn'>Generate cars</button>
+                        <button className='light-blue-btn' onClick={handleGenerateRandomCars}>Generate cars</button>
                     </div>
                 </div>
                 <div className="divider"></div>
